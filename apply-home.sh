d98787113a55a9406b3f1a8b7a0866c342608194
maypok86 <alex.mayshev.86@gmail.com>
2022-08-30 15:37:54 +0300

    Remove old files

--- apply-home.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pushd ~/projects/dotfiles
-home-manager switch -f ./home/home.nix
-popd
--- apply-system.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pushd ~/projects/dotfiles
-sudo nixos-rebuild switch -I nixos-config=./system/configuration.nix
-popd
